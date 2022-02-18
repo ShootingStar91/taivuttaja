654bf2492b5c9b531530f382457b9d0cfbd38cb3
arttu <ttu91@hotmail.com>
2022-02-18 15:43:51 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Taivuttaja
+
+A web-app for practicing conjugating Spanish verbs. Work in progress. Practice project for https://fullstackopen.com course.
+
---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div>Hola mundo</div>
+  );
+};
+
+export default App;
