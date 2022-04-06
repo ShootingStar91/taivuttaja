06fe19fc89bcbd86f51898f3123d627fc1e02f6d
arttu <ttu91@hotmail.com>
2022-04-06 23:18:15 +0300

    Fixed login validity to 12 hours after testing

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -5,7 +5,7 @@ dotenv.config();
 export const PORT = process.env.PORT || 3001;
 export const MONGODB_URI = process.env.MONGODB_URI;
 export const SECRET = process.env.SECRET;
-export const loginValidSeconds = 10;
+export const loginValidSeconds = 60 * 60 * 12;
 export const PASSWORD_MIN_LENGTH = 5;
 export const PASSWORD_MAX_LENGTH = 18;
 export const USERNAME_MIN_LENGTH = 5;
