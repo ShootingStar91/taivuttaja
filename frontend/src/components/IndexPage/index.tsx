22d7f9121682bf55e0ce82be13e62ff1686dbc37
arttu <ttu91@hotmail.com>
2022-05-06 11:19:06 +0300

    Added mode descriptions to index page

--- frontend/src/components/IndexPage/index.tsx
+++ frontend/src/components/IndexPage/index.tsx
@@ -6,9 +6,15 @@ export const IndexPage = () => {
       <h1>Welcome</h1>
       <p>Welcome to the conjugation app! Learn Spanish verbs and their conjugations here! Try the different learning modes:</p>
       <h2>Vocabulary</h2>
-      <p>Practice vocabulary by guessing what a random English verb is in Spanish.</p>
+      <p>Get a random verb in English and type it in Spanish.</p>
       <h2>Conjugate</h2>
-      <p>Practice conjugations by getting random verb to conjugate and then write out all the different forms. You can choose different moods or tenses to include in the randomed words!</p>
+      <p>Practice conjugations by getting random verb to conjugate and then write out all the different forms. You can choose different moods or tenses to include in the randomed words! Different modes for conjugating:</p>
+      <ul className='pl-4'>
+        <li><span className='font-bold'>Full mode</span>: Write all 6 forms of 1st-3rd person singular & plural</li>
+        <li><span className='font-bold'>Single mode</span>: Write one random form, for example 1st person plural</li>
+        <li><span className='font-bold'>Flashcard</span>: No typing, just guess the verb form and click to show it</li>
+      </ul>
+      <p></p>
     </div>
   );
 };
