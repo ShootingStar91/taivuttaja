e57cb1d78c3d0234df74d33e2f99a64a05cff27b
arttu <ttu91@hotmail.com>
2022-07-30 21:42:44 +0300

    style fixes in homepage and conjugation start

--- frontend/src/components/IndexPage/index.tsx
+++ frontend/src/components/IndexPage/index.tsx
@@ -3,13 +3,13 @@ import React from 'react';
 export const IndexPage = () => {
   return (
     <div>
-      <h3>Conjugation app</h3>
+      <h1>Conjugation app</h1>
       <p>Welcome! Learn Spanish verbs and their conjugations here!</p>
       <p>Click Vocab to practice <b>verb vocabulary</b>, or click Conjugate to practice <b>conjugating verbs</b> in different tenses and moods. You can also view <b>conjugation tables</b> of any verb by clicking Verbs.</p>
       <p>Create a user to set a daily goal, create wordlists, change settings and view what forms you have practiced most.</p>
       <p>Want to <b>test the functionality</b> without knowing Spanish? Open your browsers console, correct answers are printed there.</p>
 
-      <h3>About</h3>
+      <h2>About</h2>
       <p>This app is a work in progress, but most of it should work. <a className='link' href='https://github.com/ShootingStar91/taivuttaja'>Click here for the project repository</a></p>
       <p>Verb database consists of over 600 verbs from <a className='link' href='https://github.com/ghidinelli/fred-jehle-spanish-verbs'>this database</a>. Most of it is correct but <span className='font-bold'>imperative forms have errors</span>: 3rd singular and 2nd plural forms are swapped, and 1st plural form is missing. Some of this will be fixed.</p>
       <p></p>
